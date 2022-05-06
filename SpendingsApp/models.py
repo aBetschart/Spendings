037d3787def2e9fe5c660d8fc170ac18afedb054
unknown <aaron.betschart@swissonline.ch>
2022-05-06 14:55:31 +0200

    Set up the home view. ignored the database file in the git ignore file. Migrated the database base

--- SpendingsApp/models.py
+++ SpendingsApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name: models.CharField(max_length=50)
+
+class Spending(models.Model):
+    description: models.CharField(max_length=100)
+    amount: models.FloatField()
+    spendingDate: models.DateTimeField()
+    entryDate: models.DateTimeField()
+
+class SpendingCategories(models.Model):
+    spending: models.ForeignKey(Spending, on_delete=models.CASCADE, unique=True)
+    category: models.ForeignKey(Category, on_delete=models.CASCADE)
--- SpendingsApp/views.py
+++ SpendingsApp/views.py
@@ -1,3 +1,9 @@
+from turtle import rt
+from django.http import HttpRequest
 from django.shortcuts import render
+from .models import Spending
 
 
+def home(request: HttpRequest):
+    spendings = Spending.objects.all()
+    return render(request, 'home.html', {'spendings': spendings})
