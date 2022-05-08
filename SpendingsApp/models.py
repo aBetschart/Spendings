77ebac5a4ec6eefa61c01ddfbdd665bafc2df9e9
unknown <aaron.betschart@swissonline.ch>
2022-05-08 14:34:56 +0200

    Removed the migrations from the source control

--- SpendingsApp/models.py
+++ SpendingsApp/models.py
@@ -4,11 +4,8 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
 
 class Spending(models.Model):
+    entryDate = models.DateTimeField(auto_now_add=True)
+    spendingDate = models.DateField()
     description = models.CharField(max_length=100)
     amount = models.FloatField()
-    spendingDate = models.DateField()
-    entryDate = models.DateTimeField(auto_now_add=True)
-
-class SpendingCategories(models.Model):
-    spending = models.OneToOneField(Spending, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
