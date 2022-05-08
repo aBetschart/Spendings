fa4d31c946626932914ad319d8d73557296da991
unknown <aaron.betschart@swissonline.ch>
2022-05-08 14:44:53 +0200

    changed the display of a spending. Setup the __str__ functino of the category model

--- SpendingsApp/models.py
+++ SpendingsApp/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return self.name
+
 class Spending(models.Model):
     entryDate = models.DateTimeField(auto_now_add=True)
     spendingDate = models.DateField()
