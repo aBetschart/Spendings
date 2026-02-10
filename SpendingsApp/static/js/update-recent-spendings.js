b118a8058f1b7724dcb4e451312a17e93cf8713b
Blackbriar <aaron.betschart@swissonline.ch>
2026-02-10 13:30:00 +0100

    Delete and edit button on home page. Refactoring.

--- SpendingsApp/static/js/update-recent-spendings.js
+++ SpendingsApp/static/js/update-recent-spendings.js
@@ -11,17 +11,23 @@ function updateRecentSpendings() {
 
             var spendings = data.spendings;
             $.each(spendings, function(_index, spending) {
-                var date = new Date(spending.spendingDate);
-                var date_text = $.format.date(date, "dd.MM.yyyy")
-                var amount_link = '<a class = "link-underline link-underline-opacity-0"' + 
-                    'href="spending/edit/' + spending.id + '">' + spending.amount + '</a>';
-                $('#table-spendings tbody').append(
-                    '<tr>' + 
-                    "<td class='text-nowrap text-truncate'>" + date_text + '</td>' + 
-                    "<td class='text-nowrap text-truncate'>" + spending.category.name + '</td>' + 
-                    "<td class='text-nowrap text-truncate'>" + spending.description + '</td>' + 
-                    "<td class='text-end'>" + amount_link + '</td>' + 
-                    '</tr>');              
+                const date = new Date(spending.spendingDate);
+                const date_text = $.format.date(date, "dd.MM.yyyy")
+                
+                const edit_link = `spending/edit/${spending.id}`;
+                const editButton = `<a href='${edit_link}' class='btn btn-sm py-0 px-1 btn-outline-secondary'><i class='bi bi-pencil'></i></a>`;
+
+                const deleteButton = `<button class='btn btn-sm py-0 px-1 btn-outline-danger delete-spending-btn' data-spending-id='${spending.id}'><i class='bi bi-trash'></i></button>`;
+
+                const date_cell = `<td class='text-nowrap text-truncate'>${date_text}</td>`;
+                const category_cell = `<td class='text-nowrap text-truncate'>${spending.category.name}</td>`;
+                const description_cell = `<td class='text-nowrap text-truncate'>${spending.description}</td>`;
+                const amount_cell = `<td class='text-end'>${spending.amount}</td>`;
+                const edit_cell = `<td class='text-end'>${editButton}</td>`;
+                const delete_cell = `<td class='text-end'>${deleteButton}</td>`;
+
+                const row = `<tr>${date_cell}${category_cell}${description_cell}${amount_cell}${edit_cell}${delete_cell}</tr>`;
+                $('#table-spendings tbody').append(row);
             });
         }
     })
