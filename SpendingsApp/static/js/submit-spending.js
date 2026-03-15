1228591e06b05aa3719a77fac9af9c177dd02764
Blackbriar <aaron.betschart@swissonline.ch>
2026-03-15 17:31:35 +0100

    Introduced async to the spending submit process.

--- SpendingsApp/static/js/submit-spending.js
+++ SpendingsApp/static/js/submit-spending.js
@@ -4,17 +4,18 @@ import { updateRecentSpendings } from './update-recent-spendings.js';
 $(document).ready(function() {
     validateSpendingForm("#submit-spending-form");
 
-    $('#submit-spending-api-btn').mouseup(function() {
-        if ($("#submit-spending-form").valid())
-            submitSpending();
+    $('#submit-spending-api-btn').mouseup(async function() {
+        if ($("#submit-spending-form").valid()) {
+            await submitSpending();
             updateRecentSpendings();
+        }
     });
 });
 
 function submitSpending() {
     var formData = new FormData(document.getElementById('submit-spending-form'));
     
-    $.ajax({
+    return $.ajax({
         type: "POST",
         url: DJANGO_URLS.spending_submit_api,
         data: formData,
@@ -23,13 +24,13 @@ function submitSpending() {
         success: function(data) {
             console.log("Spending submitted successfully:", data);
             $("#submit-spending-form")[0].reset();
-            updateRecentSpendings();
         },
         error: function(xhr, status, error) {
             console.error("Error status:", xhr.status);
             console.error("Error response:", xhr.responseText);
             console.error("Error message:", error);
             alert("Error submitting spending. Please check the console.");
+            throw new Error("Submission failed");
         }
     });
 }
