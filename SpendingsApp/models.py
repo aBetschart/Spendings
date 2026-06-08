635244c0a3d54e0842bb6cfaafa3e112074074cb
Blackbriar <aaron.betschart@swissonline.ch>
2026-06-08 14:35:28 +0200

    Changed models for multi user, step 1.
    Models do now have a foreign key to a user and migrations tie it to me as default user.

--- SpendingsApp/models.py
+++ SpendingsApp/models.py
@@ -1,7 +1,12 @@
+
+
 from django.db import models
+from django.conf import settings
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, related_name='categories')
 
     def __str__(self) -> str:
         return self.name
@@ -12,6 +17,7 @@ class Spending(models.Model):
     description = models.CharField(max_length=100)
     amount = models.FloatField()
     category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, related_name='spendings')
 
     def __str__(self) -> str:
         date = self.spendingDate.strftime('%d.%m.%y')
